da7112c3dc7c58063f54048698d92883321b3481
Rashaun Godding <90447626+rgodding@users.noreply.github.com>
2026-05-20 20:58:27 +0200

    push

--- StudyCentral.API/Controllers/UserController/AuthController.cs
+++ StudyCentral.API/Controllers/UserController/AuthController.cs
@@ -25,7 +25,6 @@ public class AuthController : BaseUserController
     [Route("get-user-info")]
     public async Task<IActionResult> GetUserInfo()
     {
-        Console.WriteLine("Getting user info");
         var user = await _userService.GetUserInfo(UserPrincipal.Id);
         var response = _mapper.Map<UserDto>(user);
         return Ok(response);
