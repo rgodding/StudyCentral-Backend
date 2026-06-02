f9ffaad9795123128e2c475a353ce9a006c551cc
Rashaun Godding <90447626+rgodding@users.noreply.github.com>
2026-06-02 17:39:06 +0200

    StudentCourse done

---
+++ StudyCentral.API/Controllers/Student/BaseStudentController.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+
+namespace StudyCentral.API.Controllers.Student;
+
+[Authorize(Policy = "IsStudent")]
+public class BaseStudentController : BaseUserController
+{
+    public BaseStudentController(IMapper mapper) : base(mapper)
+    {
+    }
+}
---
+++ StudyCentral.API/Controllers/Student/StudentCourseController.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using StudyCentral.API.Models.Dtos.Courses;
+using StudyCentral.API.Services;
+
+namespace StudyCentral.API.Controllers.Student;
+
+[ApiController]
+[Route("api/teacher/[controller]")]
+public class StudentCourseController : BaseStudentController
+{
+    
+    private readonly ICourseService _courseService;
+    
+    public StudentCourseController(IMapper mapper, ICourseService courseService) : base(mapper)
+    {
+        _courseService = courseService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetCourses()
+    {
+        var result = await _courseService.GetAllCourses();
+        var response = _mapper.Map<List<CourseDto>>(result);
+        return Ok(response);
+    }
+    
+    [HttpGet]
+    [Route("{courseId}")]
+    public async Task<IActionResult> GetCourseById(Guid courseId)
+    {
+        var result = await _courseService.GetCourse(courseId);
+        var response = _mapper.Map<CourseDto>(result);
+        return Ok(response);
+    }
+}
