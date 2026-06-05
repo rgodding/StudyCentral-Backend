3941127299b82baea87428a782608ef617476220
Rashaun Godding <90447626+rgodding@users.noreply.github.com>
2026-06-05 17:50:30 +0200

    remove readme

---
+++ StudyCentral.API/Services/AssignmentPreviewDto.cs
@@ -0,0 +1,13 @@
+﻿namespace StudyCentral.API.Services;
+
+public class AssignmentPreviewDto
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; } = null!;
+    public DateTime? Deadline { get; set; }
+
+    public Guid CourseId { get; set; }
+    public string CourseTitle { get; set; } = null!;
+
+    public int FileCount { get; set; }
+}
