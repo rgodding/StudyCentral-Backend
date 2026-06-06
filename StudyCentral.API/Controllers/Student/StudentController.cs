ff659ae4e8edcc2a3b51a8d36c57d158a77d05f6
Rashaun Godding <90447626+rgodding@users.noreply.github.com>
2026-06-06 04:37:25 +0200

    studentbase

--- StudyCentral.API/Controllers/Student/StudentController.cs
+++ StudyCentral.API/Controllers/Student/StudentController.cs
@@ -1,6 +1,12 @@
-﻿namespace StudyCentral.API.Controllers.Student;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 
-public class StudentController
+namespace StudyCentral.API.Controllers.Student;
+
+[Authorize(Roles = "Student")]
+public abstract class StudentController : BaseController
 {
-    
+    protected StudentController(IMapper mapper) : base(mapper)
+    {
+    }
 }
